604e8cc631d046a774ac59646651725a4d0849dc
Ewelina Szoda <ewelinaszoda@gmail.com>
2020-04-14 20:07:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,3 @@
 def reverse_each_word(sentence) 
-  sentence_array = sentence.split("")  #sentence.chars
-  sentence_array_reverse = sentence_array.each { |element| element.reverse }
-  sentence_array_reverse.join(" ")
+ sentence.split.collect { |word| word.reverse }.join
 end 
