666741ad67067d6ed55f19ce8b17a0c4118be8b1
Ewelina Szoda <ewelinaszoda@gmail.com>
2020-04-14 20:01:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,5 @@
 def reverse_each_word(sentence) 
-  sentence.split("")
+  sentence_array = sentence.split("")  #sentence.chars
+  sentence_array_reverse = sentence_array.each { |element| element.reverse }
+  sentence_array_reverse.join(" ")
 end 
